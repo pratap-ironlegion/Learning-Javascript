bdaab8cfe08d391985349e6fdffc2fc505033e14
Pratap <pratap@gmail.com>
2026-01-14 11:27:52 +0530

    time interval changes

--- 4.Time Intervals/script2.js
+++ 4.Time Intervals/script2.js
@@ -1,12 +1,12 @@
 let progressbar = document.querySelector("#progressBar");
 let percenttext = document.querySelector("#percentText");
 let startBtn = document.querySelector("#startBtn");
+let h2 = document.querySelector("h2");
 
 startBtn.addEventListener("click", function(dets) {
+    h2.textContent = "Downloading...";
     let count = 0;
     const interval = setInterval(function() {
-        console.log(count);
-     
 
         if (count <= 99) {
             count++;
@@ -14,6 +14,7 @@ startBtn.addEventListener("click", function(dets) {
             percenttext.textContent = `${count}%`;
         }
         else {
+            h2.innerHTML = "<b>Download Completed.</b>";
             clearInterval(interval);
         }
     }, 10000/ 100);
